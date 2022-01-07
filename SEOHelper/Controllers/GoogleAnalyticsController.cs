6bdc3ba119cf8d7f5b91b7491b595e4b7f2ad7ce
ZairiAimenDz <zairiaimendev@gmail.com>
2022-01-07 14:31:26 +0100

    minor changes

---
+++ SEOHelper/Controllers/GoogleAnalyticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEOHelper.Controllers
+{
+    public class GoogleAnalyticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
